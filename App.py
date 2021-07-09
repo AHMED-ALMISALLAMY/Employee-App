2a426eb8a7d56525532a66d890734c7f1d134e2a
Ahmed Almisallamy <78504402+AHMED-ALMISALLAMY@users.noreply.github.com>
2021-07-09 16:43:39 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,7 @@
 from employee import Employee
 
-employee_1 = Employee("Ahmed",78274,15,"Facebook",False)
+employee_1 = Employee("Ahmed",78274,15,"Facebook",120000,4,False)
 
 employee_1.view_details()
+
+employee_1.bonus()
