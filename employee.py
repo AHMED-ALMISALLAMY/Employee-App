421b4e634957b263979306252e888e61b01d3737
Ahmed Almisallamy <78504402+AHMED-ALMISALLAMY@users.noreply.github.com>
2021-07-09 16:42:40 +0200

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,9 +1,11 @@
 class Employee:
-    def __init__(self, name, id, age, departement, is_manager) -> None:
+    def __init__(self, name, id, age, departement,salary,experiance, is_manager) -> None:
         self.name = name
         self.id = id
         self.age = age
         self.departement = departement
+        self.salary = salary
+        self.experiance = experiance
         self.is_manager = is_manager
 
     def view_details(self):
@@ -12,4 +14,14 @@ class Employee:
         print("ID:", self.id)
         print("Age:", self.age)
         print("Departement:", self.departement)
+        print("Experiance is:", self.experiance)
+        print("Your salary:", self.salary)
         print("Is Manager =>", self.is_manager)
+        
+    def bonus(self):
+        if self.experiance > 3:
+            self.salary += 20000
+            print("Your Salary Increase To " + str(self.salary) + " per year")
+        else:
+            print("Your salary is " + str(self.salary) + " per year")
+        
